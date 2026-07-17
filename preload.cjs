e172a483dfcf988c7cc38cbaa827d4a77741c9ad
alimozaffari-stack <ali0mozaffari@gmail.com>
2026-07-17 10:30:19 +1000

    Allow multiple workspace files and folders

--- preload.cjs
+++ preload.cjs
@@ -6,6 +6,7 @@ contextBridge.exposeInMainWorld("launcher", {
   saveState: (state) => ipcRenderer.invoke("library:save", state),
   openItem: (item) => ipcRenderer.invoke("library:open", item),
   chooseResource: (kind) => ipcRenderer.invoke("library:choose-resource", kind),
+  chooseResources: (kind) => ipcRenderer.invoke("library:choose-resources", kind),
   scanFolder: (folderPath) => ipcRenderer.invoke("library:scan-folder", folderPath),
   getIcon: (target) => ipcRenderer.invoke("library:get-icon", target),
   pathExists: (target) => ipcRenderer.invoke("library:path-exists", target),
